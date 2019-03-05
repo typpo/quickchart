57d0bc9e3b1461dc34cc5fe90fe6637973103c19
Ian Webster <ianw_github@ianww.com>
2019-03-04 16:23:12 -0800

    update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -e
 
-sudo apt-get install -y libcairo2-dev libjpeg-dev libgif-dev libpango1.0-dev
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+
+sudo apt-get update
+sudo apt-get install -y libcairo2-dev libjpeg-dev libgif-dev libpango1.0-dev python nodejs yarn
+
+npm install -g pm2
