f559daecfbac083eece502a0cc2dd76d1cfdb6fe
Ian Webster <ianw_github@ianww.com>
2019-08-27 10:42:53 -0700

    Add MIT license for uuid code

--- cloudflare/create_shorturl.js
+++ cloudflare/create_shorturl.js
@@ -1,38 +1,35 @@
-addEventListener('fetch', (event) => {
+addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request));
 });
 
-const CHART_TTL = 60 * 60 * 24 * 30 * 3;
-
-function fail(msg) {
-  return new Response(
-    JSON.stringify({
-      success: false,
-      error: msg,
-    }),
-    {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    },
-  );
-}
-
-function success(data) {
-  return new Response(
-    JSON.stringify(
-      Object.assign(
-        {
-          success: true,
-        },
-        data,
-      ),
-    ),
-    {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    },
-  );
-}
+/******************************************************************************
+ * The code below, bytesToUuid, and the uuidv4 function are adapted from
+ * node-uuid (https://github.com/kelektiv/node-uuid/blob/master/v4.js) with the
+ * following license.  Note that the remainder of the project is licensed under
+ * GNU GPLv3 (see https://github.com/typpo/quickchart/blob/master/LICENSE).
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2010-2016 Robert Kieffer and other contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *****************************************************************************/
 
 const byteToHex = [];
 for (let i = 0; i < 256; ++i) {
@@ -68,7 +65,6 @@ function bytesToUuid(buf) {
 }
 
 function uuidv4() {
-  // From node-uuid https://github.com/kelektiv/node-uuid/blob/master/v4.js
   const rnds = crypto.getRandomValues(new Uint8Array(16));
 
   // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`
@@ -76,6 +72,42 @@ function uuidv4() {
   rnds[8] = (rnds[8] & 0x3f) | 0x80;
 
   return bytesToUuid(rnds);
+}
+/******************************************************************************
+ * End MIT licensed code. All other code licensed under GNU GPLv3.
+ *****************************************************************************/
+
+// 3 months in seconds
+const CHART_TTL = 60 * 60 * 24 * 30 * 3;
+
+function fail(msg) {
+  return new Response(
+    JSON.stringify({
+      success: false,
+      error: msg,
+    }),
+    {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    },
+  );
+}
+
+function success(data) {
+  return new Response(
+    JSON.stringify(
+      Object.assign(
+        {
+          success: true,
+        },
+        data,
+      ),
+    ),
+    {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    },
+  );
 }
 
 /**
