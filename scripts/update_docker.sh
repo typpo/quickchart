7b4d30a99bc7baa35bd2e6d3bf7b4f575fc843e3
Ian Webster <ianw_github@ianww.com>
2019-09-09 10:49:56 -0700

    Docker script should update version as well

--- scripts/update_docker.sh
+++ scripts/update_docker.sh
@@ -4,8 +4,22 @@ pushd $(dirname $0) &>/dev/null
 
 cd ..
 
+if [ $# -ne 1 ]; then
+  echo 'Usage: ./update_docker.sh <version>'
+  exit 1
+fi
+
+VERSION=$1
+
+echo 'Updating latest...'
+
 docker build -t ianw/quickchart .
 docker push ianw/quickchart
+
+echo "Updating version ${VERSION}"
+
+docker build -t ianw/quickchart:${VERSION} .
+docker push ianw/quickchart:${VERSION}
 
 popd &>/dev/null
 
