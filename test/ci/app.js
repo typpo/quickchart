e6f0f045b34dc2f6b307a58a718f8c1c76883195
Ian Webster <ianw_github@ianww.com>
2019-10-23 18:40:18 -0400

    Add some tests for base64 encoding

--- test/ci/app.js
+++ test/ci/app.js
@@ -18,8 +18,8 @@ function assertAlmostEqualNumbers(x, y, tolerance = 10) {
   }
 }
 
-describe('chart requests', () => {
-  it('return a basic chart via GET', done => {
+describe('chart request', () => {
+  it('returns a basic chart via GET', done => {
     request(app)
       .get(`/chart?c=${encodeURIComponent(JSON.stringify(BASIC_CHART))}`)
       .expect('Content-Type', 'image/png')
@@ -32,7 +32,22 @@ describe('chart requests', () => {
       });
   });
 
-  it('return an JS chart via GET', done => {
+  it('returns a basic chart via GET, base64 encoded', done => {
+    request(app)
+      .get(
+        `/chart?c=${Buffer.from(JSON.stringify(BASIC_CHART)).toString('base64')}&encoding=base64`,
+      )
+      .expect('Content-Type', 'image/png')
+      .expect(200)
+      .end((err, res) => {
+        const dimensions = imageSize(res.body);
+        assert.equal(500 * 2, dimensions.width);
+        assert.equal(300 * 2, dimensions.height);
+        done();
+      });
+  });
+
+  it('returns an JS chart via GET', done => {
     request(app)
       .get(`/chart?c=${encodeURIComponent(JS_CHART)}`)
       .expect('Content-Type', 'image/png')
@@ -45,7 +60,7 @@ describe('chart requests', () => {
       });
   });
 
-  it('return a basic chart via GET with parameters', done => {
+  it('returns a basic chart via GET with parameters', done => {
     request(app)
       .get(
         `/chart?c=${encodeURIComponent(
@@ -67,7 +82,7 @@ describe('chart requests', () => {
       });
   });
 
-  it('return a basic chart via POST', done => {
+  it('returns a basic chart via POST', done => {
     request(app)
       .post('/chart')
       .send({
@@ -83,7 +98,24 @@ describe('chart requests', () => {
       });
   });
 
-  it('return an advanced chart via POST', done => {
+  it('returns a basic chart via POST, base64 encoded', done => {
+    request(app)
+      .post('/chart')
+      .send({
+        chart: Buffer.from(JSON.stringify(BASIC_CHART)).toString('base64'),
+        encoding: 'base64',
+      })
+      .expect('Content-Type', 'image/png')
+      .expect(200)
+      .end((err, res) => {
+        const dimensions = imageSize(res.body);
+        assert.equal(500 * 2, dimensions.width);
+        assert.equal(300 * 2, dimensions.height);
+        done();
+      });
+  });
+
+  it('returns an advanced chart via POST', done => {
     request(app)
       .post('/chart')
       .send({
@@ -99,7 +131,7 @@ describe('chart requests', () => {
       });
   });
 
-  it('return an advanced chart via POST with parameters', done => {
+  it('returns an advanced chart via POST with parameters', done => {
     request(app)
       .post('/chart')
       .send({
@@ -120,6 +152,56 @@ describe('chart requests', () => {
         const dimensions = imageSize(res.body);
         assert.equal(456, dimensions.width);
         assert.equal(123, dimensions.height);
+        done();
+      });
+  });
+
+  it('returns an advanced chart via POST with parameters and base 64', done => {
+    request(app)
+      .post('/chart')
+      .send({
+        chart: Buffer.from(JS_CHART).toString('base64'),
+        width: 369,
+        height: 150,
+        devicePixelRatio: 1.0,
+        backgroundColor: 'rgb(190, 180, 170)',
+        encoding: 'base64',
+      })
+      .expect('Content-Type', 'image/png')
+      .expect(200)
+      .end((err, res) => {
+        const rgb = colorThief.getColor(res.body);
+        assertAlmostEqualNumbers(190, rgb[0]);
+        assertAlmostEqualNumbers(180, rgb[1]);
+        assertAlmostEqualNumbers(170, rgb[2]);
+
+        const dimensions = imageSize(res.body);
+        assert.equal(369, dimensions.width);
+        assert.equal(150, dimensions.height);
+        done();
+      });
+  });
+
+  it('reverts correctly to background transparency', done => {
+    // Don't let background selection stick between requests.
+    request(app)
+      .post('/chart')
+      .send({
+        chart: BASIC_CHART,
+      })
+      .expect('Content-Type', 'image/png')
+      .expect(200)
+      .end((err, res) => {
+        const rgb = colorThief.getColor(res.body);
+        // Image is transparent by default - expect dominant color to be blue
+        // bars.
+        assertAlmostEqualNumbers(52, rgb[0]);
+        assertAlmostEqualNumbers(164, rgb[1]);
+        assertAlmostEqualNumbers(236, rgb[2]);
+
+        const dimensions = imageSize(res.body);
+        assert.equal(500 * 2, dimensions.width);
+        assert.equal(300 * 2, dimensions.height);
         done();
       });
   });
