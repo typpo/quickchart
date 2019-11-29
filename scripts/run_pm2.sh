7adc1b4b82c48b1c9ac9669e25a75b4577f9b8e6
Ian Webster <ianw_github@ianww.com>
2019-11-29 10:16:25 -0500

    Reduce public rate limit

--- scripts/run_pm2.sh
+++ scripts/run_pm2.sh
@@ -4,6 +4,6 @@ pushd $(dirname $0) &>/dev/null
 
 pm2 stop quickchart || true
 pm2 delete quickchart || true
-NODE_ENV=production ENABLE_TELEMETRY_WRITE=1 DISABLE_TELEMETRY=1 RATE_LIMIT_PER_MIN=240 pm2 start ../index.js --name 'quickchart' -i max --node-args="--max-http-header-size=65536"
+NODE_ENV=production ENABLE_TELEMETRY_WRITE=1 DISABLE_TELEMETRY=1 RATE_LIMIT_PER_MIN=120 pm2 start ../index.js --name 'quickchart' -i max --node-args="--max-http-header-size=65536"
 
 popd &>/dev/null
