3a1af4ae29f2a36f0ce77f65793079d3ded66015
Ian Webster <ianw_github@ianww.com>
2019-12-22 23:58:13 -0500

    Switch to bunyan logging and enable Stackdriver

--- logging.js
+++ logging.js
@@ -1,8 +1,27 @@
-const winston = require('winston');
+const path = require('path');
 
-const logger = new winston.Logger({
-  level: process.env.LOG_LEVEL || 'info',
-  transports: [new winston.transports.Console({ timestamp: true, colorize: true })],
+const bunyan = require('bunyan');
+const { LoggingBunyan } = require('@google-cloud/logging-bunyan');
+
+const loggingBunyan = new LoggingBunyan({
+  projectId: process.env.STACKDRIVER_PROJECT_ID,
+  keyFilename: process.env.STACKDRIVER_KEYFILE,
+});
+
+function getStreams() {
+  const streams = [{ stream: process.stdout, level: process.env.LOG_LEVEL }];
+  if (process.env.NODE_ENV === 'production' && process.env.STACKDRIVER_PROJECT_ID) {
+    console.log('Using stackdriver logging!');
+    streams.push(loggingBunyan.stream('debug'));
+  } else {
+    console.log('Stackdriver logging is disabled');
+  }
+  return streams;
+}
+
+const logger = bunyan.createLogger({
+  name: 'quickchart',
+  streams: getStreams(),
 });
 
 module.exports = {
