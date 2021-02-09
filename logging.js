b45c6a9fc798a353bd5d4f7abdd1f97d2a156ddc
Ian Webster <ianw_github@ianww.com>
2021-02-09 08:02:49 -0800

    Remove google logging dependency

--- logging.js
+++ logging.js
@@ -1,27 +1,8 @@
-const path = require('path');
-
 const bunyan = require('bunyan');
-const { LoggingBunyan } = require('@google-cloud/logging-bunyan');
-
-const loggingBunyan = new LoggingBunyan({
-  projectId: process.env.STACKDRIVER_PROJECT_ID,
-  keyFilename: process.env.STACKDRIVER_KEYFILE,
-});
-
-function getStreams() {
-  const streams = [{ stream: process.stdout, level: process.env.LOG_LEVEL }];
-  if (process.env.NODE_ENV === 'production' && process.env.STACKDRIVER_PROJECT_ID) {
-    console.log('Using stackdriver logging!');
-    streams.push(loggingBunyan.stream('debug'));
-  } else {
-    console.log('Stackdriver logging is disabled');
-  }
-  return streams;
-}
 
 const logger = bunyan.createLogger({
   name: 'quickchart',
-  streams: getStreams(),
+  streams: [{ stream: process.stdout, level: process.env.LOG_LEVEL }],
 });
 
 module.exports = {
