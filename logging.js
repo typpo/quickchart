c2d2ba55fa3d547cba56bd27aae115eb00ffef1e
Paul McCartney <pmccartney@new-innov.com>
2024-04-19 11:56:09 -0400

    fix: add plugins for chart js 3

--- logging.js
+++ logging.js
@@ -1,8 +1,17 @@
-const bunyan = require('bunyan');
+const pino = require('pino');
 
-const logger = bunyan.createLogger({
-  name: 'quickchart',
-  streams: [{ stream: process.stdout, level: process.env.LOG_LEVEL }],
+const logger = pino({
+  // setting this to undefined prevents pino from adding hostname
+  base: undefined,
+  messageKey: 'message',
+  // replace the # level with the level string
+  formatters: {
+    level(/** @type {any} */ level) {
+      return { level };
+    },
+  },
+  // eslint-disable-next-line turbo/no-undeclared-env-vars
+  mixin: () => ({ name: `quickcharts` }),
 });
 
 module.exports = {
